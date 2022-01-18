d4a2e2248e7e0c38b06a4f4816df34a0b26507c9
Vladimir G. Galkin <v.g.galkin@gmail.com>
2022-01-19 01:04:01 +0300

    Add Load Balancer

--- main.tf
+++ main.tf
@@ -5,6 +5,18 @@ terraform {
       version = "0.70.0"
     }
   }
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "tf-state-bucket-vladimir"
+    region     = "ru-central1-a"
+    key        = "issue1/lemp.tfstate"
+    access_key = "RVn3B0SsGPncuvOcNvba"
+    secret_key = "Xpq1KaDIDtaYbkVE-bNpBFqDX2pRb8fN_xkOzwJq"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 // Configure the Yandex.Cloud provider
@@ -15,50 +27,70 @@ provider "yandex" {
   zone                     = var.zone_a
 }
 
-data "yandex_compute_image" "lemp_image" {
-  family = "lemp"
+resource "yandex_vpc_network" "network" {
+  name = "network"
 }
 
-resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
-  zone = var.zone_a
-  
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.lemp_image.id
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
-  }
-}
-
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
+resource "yandex_vpc_subnet" "subnet1" {
   name           = "subnet1"
   zone           = var.zone_a
-  network_id     = yandex_vpc_network.network-1.id
+  network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+//resource "yandex_vpc_subnet" "subnet2" {
+//  name           = "subnet2"
+//  zone           = var.zone_b
+//  network_id     = yandex_vpc_network.network.id
+//  v4_cidr_blocks = ["192.168.11.0/24"]
+//}
+
+module "ya_instance_1" {
+  source                = "./modules/instance"
+  instance_family_image = "lemp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet1.id
 }
 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+module "ya_instance_2" {
+  source                = "./modules/instance"
+  instance_family_image = "lamp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet1.id
+}
+
+resource "yandex_lb_target_group" "target_group" {
+  name      = "my-target-group"
+  region_id = "ru-central1"
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    address   = module.ya_instance_1.internal_ip_address_vm
+  }
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    address   = module.ya_instance_2.internal_ip_address_vm
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "load_balancer" {
+  name = "my-network-load-balancer"
+
+  listener {
+    name = "my-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.target_group.id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+      }
+    }
+  }
 }
---
+++ modules/instance/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.70.0"
+    }
+  }
+}
+
+data "yandex_compute_image" "my_image" {
+  family = var.instance_family_image
+}
+
+resource "yandex_compute_instance" "vm" {
+  name = "terraform-${var.instance_family_image}"
+
+  resources {
+      cores = 2
+      memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.my_image.id
+    }
+  }
+
+  network_interface {
+    subnet_id = var.vpc_subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
