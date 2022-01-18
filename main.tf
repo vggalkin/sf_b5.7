568851ffa5a7de0cf2d7ab4eeee2bb83df154bd4
Vladimir G. Galkin <v.g.galkin@gmail.com>
2022-01-19 01:11:40 +0300

    Add Load Balancer

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
     bucket     = "tf-state-bucket-vladimir"
     region     = "ru-central1-a"
     key        = "issue1/lemp.tfstate"
-    access_key = "RVn3B0SsGPncuvOcNvba"
-    secret_key = "Xpq1KaDIDtaYbkVE-bNpBFqDX2pRb8fN_xkOzwJq"
+    access_key = "<access_key>"
+    secret_key = "<secret_key>"
 
     skip_region_validation      = true
     skip_credentials_validation = true
@@ -37,13 +37,6 @@ resource "yandex_vpc_subnet" "subnet1" {
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-
-//resource "yandex_vpc_subnet" "subnet2" {
-//  name           = "subnet2"
-//  zone           = var.zone_b
-//  network_id     = yandex_vpc_network.network.id
-//  v4_cidr_blocks = ["192.168.11.0/24"]
-//}
 
 module "ya_instance_1" {
   source                = "./modules/instance"
